c30c73dceb546057de4c0310433604c012b99c58
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2021-09-13 02:07:30 +0800

    add extra credits in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,6 +5,10 @@ function Footer() {
     <footer>
       <p>&copy; Emman Evangelista 2021.</p>
       <p>Developed as part of The GUIDON's 2021 application requirements.</p>
+      <p>
+        Logos and colors taken from the original website. Images are from
+        Unsplash. Fonts are from Google Fonts.
+      </p>
     </footer>
   );
 }
