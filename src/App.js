805d4ddd5a4bc902271416a50395faf18597c288
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2021-09-12 15:46:21 +0800

    add main component containers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
