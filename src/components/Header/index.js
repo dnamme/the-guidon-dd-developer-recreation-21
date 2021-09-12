63af29d65c4e67c455801866dca37179bc7f35b6
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2021-09-12 16:04:52 +0800

    add content and basic styles for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,20 @@
+import logo from "./../../img/logo-white.png";
+import "./styles.css";
+
 function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <div id="bg">
+        <img src="https://lorempixel.com/1920/1080" alt="placeholder" />
+      </div>
+      <div id="content">
+        <img src={logo} alt="The GUIDON logo" />
+        <h1>Hold the Truth to Power.</h1>
+        <h2>COA RECWEEK 2021</h2>
+        <h2>SEPTEMBER 6-18</h2>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
